0982a0515d702f077f21acb916c98d4f4828988e
Samuel García <samuel.garcia@tum.de>
2025-12-22 21:15:32 +0000

    Correctly load envvars in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-source $(pwd)/kg_construction_and_validation/.env
-
 CONTEXT=$(pwd) \
 DEPLOYMENT_PATH=$(pwd)/deployment \
 VIRTUOSO_PATH=$(pwd)/virtuoso \
 HANDOVER_WORKFLOWS_UI_DOCKERFILE=$(pwd)/deployment/webui.dockerfile \
 RDF_DATASTORE_API_DOCKERFILE=$(pwd)/deployment/rdf_datastore_api.dockerfile \
 MATERIALIZATION_DOCKERFILE=$(pwd)/deployment/materialization.dockerfile \
-docker compose -f deployment/virtuoso_deployment.yml up --build
+docker compose --env-file ./kg_construction_and_validation/.env -f deployment/virtuoso_deployment.yml up --build
