fcdecd38b3b1008f68abc54b33324e96b6685f90
Sondeluz <git@samueldgv.com>
2026-02-27 18:03:42 +0100

    Do not allow renaming a step into one that already exists (but properly now)

--- kg_construction_and_validation/handover_workflows_validation_webui/workflow_model_ui/workflow_model_step_controls.py
+++ kg_construction_and_validation/handover_workflows_validation_webui/workflow_model_ui/workflow_model_step_controls.py
@@ -54,7 +54,7 @@ def change_step_name_action(new_name: str,
         ui.notify("No step selected", type='warning')
         return
 
-    if not new_name in app.storage.tab['current_workflow_model'].workflow_model_steps:
+    if new_name in app.storage.tab['current_workflow_model'].workflow_model_steps:
         ui.notify("There is already another step with this name", type='negative')
         return
 
